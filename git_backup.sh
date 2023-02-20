05f01b322e793417b4e8445693d68a160bbac434
scuk23 <contactjakep@gmail.com>
2023-02-20 17:19:04 -0500

    Backup triggered on 2023-02-20 17:19:04
    Klipper on commit: 848a78d
    
    Moonraker on commit: 0a811b9
    
    Mainsail version: v2.5.0-beta1

--- git_backup.sh
+++ git_backup.sh
@@ -66,7 +66,7 @@ push_config(){
   git add . -v
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push "https://scuk23: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@github.com/scuk23/LDOvoron2.4r2"
+  git push "https://scuk23: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@github.com/scuk23/LDOvoron2.4r2"
 #  git push "git@github.com:scuk23/LDOvoron2.4r2.git"
 }
 
